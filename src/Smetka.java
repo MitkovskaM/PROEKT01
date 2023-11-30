3be63635f5b276d1ee8b2d135ea9b274aa11cf82
MitkovskaM <mihaela.mitkovska@gmail.com>
2023-11-30 09:33:40 +0100

    Add first java proekt

---
+++ src/Smetka.java
@@ -0,0 +1,15 @@
+
+public class Smetka {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int j=32;
+int k=20;
+int p=50;
+double rez;
+rez= (j*2)+(k*5)+(p*2.5);
+System.out.println("Vkupno vo denari");
+System.out.println(rez);
+	}
+
+}
